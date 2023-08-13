b8b8dc8d1616b2b850478bf5416e564b80829e07
PckProgramador <958patrick@gmail.com>
2023-08-13 13:32:05 +0200

    primer commit 1

---
+++ ejercicio1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ejercicio1{
+    public static void main(String[] args) {
+        System.out.println("dime tu nombre");
+        String nombre = new Scanner(System.in).nextLine();
+        System.out.println("dime tu primer apellido");
+        String apellido1 = new Scanner(System.in).nextLine();
+        System.out.println("dime tu segundo apellido");
+        String apellido2 = new Scanner(System.in).nextLine();
+        System.out.println("dime tu dni");
+        String dni = new Scanner(System.in).nextLine();
+        System.out.println("dime tu edad");
+        int edad = new Scanner(System.in).nextInt();
+        
+
+        System.out.println("Eres "+ nombre + apellido1 + apellido2 + ",tu dni es "+ dni + " y tu edad es "+ edad);
+        
+    }
+}
